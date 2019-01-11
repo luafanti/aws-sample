3df285f567584d4b3482bd17ad9b2e33846895b9
lua <luafanti@gmail.com>
2019-01-11 20:56:04 +0100

    Emails service first step

--- src/main/java/pl/lua/aws/core/service/EmailService.java
+++ src/main/java/pl/lua/aws/core/service/EmailService.java
@@ -1,4 +1,90 @@
 package pl.lua.aws.core.service;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+
+    // Replace sender@example.com with your "From" address.
+    // This address must be verified with Amazon SES.
+    static final String FROM = "woodenpoker@gmail.com";
+
+    // Replace recipient@example.com with a "To" address. If your account
+    // is still in the sandbox, this address must be verified.
+    static final String TO = "rafisrafikombain@gmail.com";
+
+    // The configuration set to use for this email. If you do not want to use a
+    // configuration set, comment the following variable and the
+    // .withConfigurationSetName(CONFIGSET); argument below.
+    static final String CONFIGSET = "ConfigSet";
+
+    // The subject line for the email.
+    static final String SUBJECT = "Amazon SES test (AWS SDK for Java)";
+
+    // The HTML body for the email.
+    static final String HTMLBODY = "<h1>Amazon SES test (AWS SDK for Java)</h1>"
+            + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+            + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+            + "AWS SDK for Java</a>";
+
+    // The email body for recipients with non-HTML email clients.
+    static final String TEXTBODY = "This email was sent through Amazon SES "
+            + "using the AWS SDK for Java.";
+
+//
+//    @Autowired
+//    AWSCredentialsProvider credentialsProvider;
+
+
+    public void sendMail(){
+
+        try {
+            AmazonSimpleEmailService client =
+                    AmazonSimpleEmailServiceClientBuilder.standard()
+                            // Replace US_WEST_2 with the AWS Region you're using for
+                            // Amazon SES.
+                            .withRegion(Regions.US_WEST_2).build();
+            SendEmailRequest request = new SendEmailRequest()
+                    .withDestination(
+                            new Destination().withToAddresses(TO))
+                    .withMessage(new Message()
+                            .withBody(new Body()
+                                    .withHtml(new Content()
+                                            .withCharset("UTF-8").withData(HTMLBODY))
+                                    .withText(new Content()
+                                            .withCharset("UTF-8").withData(TEXTBODY)))
+                            .withSubject(new Content()
+                                    .withCharset("UTF-8").withData(SUBJECT)))
+                    .withSource(FROM);
+                    // Comment or remove the next line if you are not using a
+                    // configuration set
+                   // .withConfigurationSetName(CONFIGSET);
+            client.sendEmail(request);
+            System.out.println("Email sent!");
+        } catch (Exception ex) {
+            System.out.println("The email was not sent. Error message: "
+                    + ex.getMessage());
+        }
+    }
+
+
+//
+//
+//
+//        AmazonSimpleEmailService sesService = AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(credentialsProvider).withRegion(Regions.US_WEST_2).build();
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//        simpleMailMessage.setFrom("luafanti@gmail.com");
+//        simpleMailMessage.setTo("rafisrafikombain@gmail.com");
+//        simpleMailMessage.setSubject("subject");
+//        simpleMailMessage.setText("text");
+//
+////        RawMessage rawMessage = new RawMessage(ByteBuffer.wrap(outputStream.toByteArray()));
+//        SendRawEmailRequest rawEmailRequest = new SendRawEmailRequest(simpleMailMessage).withDestinations(Arrays.asList(recipient));
+//        SendEmailRequest emailRequest - new SendEmailRequest("ZXc","Zxc",simpleMailMessage);
+//        sesService.sendRawEmail(rawEmailRequest);
+
 }
